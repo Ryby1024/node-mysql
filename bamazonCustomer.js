e729490d7e921ff4fef8e777700f98785b5d9da9
Roy Davis <myroy1024@yahoo.com>
2019-12-05 14:16:28 -0500

    Almost there with the first part. Getting one error on the calculating total

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,81 +1,97 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 var connection = mysql.createConnection({
-  host: "localhost",
+    host: "localhost",
 
-  // Your port; if not 3306
-  port: 3306,
+    // Your port; if not 3306
+    port: 3306,
 
-  // Your username
-  user: "root",
+    // Your username
+    user: "root",
 
-  // Your password
-  password: "Bribrenbray1",
-  database: "bamazon_db"
+    // Your password
+    password: "Bribrenbray1",
+    database: "bamazon_db"
 });
 
-connection.connect(function(err) {
-  if (err) throw err;
-  display();
-  userBuy();
-  
+connection.connect(function (err) {
+    if (err) throw err;
+    display();
+    userBuy();
+    connection.end();
 });
-
-function display(){
-    connection.query("SELECT * FROM products", function(err, results) {
+function display() {
+    connection.query("SELECT * FROM products", function (err, results) {
         if (err) throw err;
-   
-   var choiceArray = [];
-   for(let i = 0; i < results.length; i++ ){
-       choiceArray.push("\n" +"Item ID: " + results[i].item_id + " "+ "Item Name: " + results[i].product_name + " "+ "Item Price: " + results[i].price);
-   }
-   console.log(choiceArray.toString());
-})
+        console.log("--------------------");
+        console.log("ID | Product Name | Price ");
+        console.log("--------------------")
+
+        for (let i = 0; i < results.length; i++) {
+            console.log(results[i].item_id + " | " + results[i].product_name + " | " + results[i].price);
+        }
+
+    })
 }
 
+
 function userBuy() {
+    connection.query("SELECT * FROM products", function (err, results) {
+        if (err) throw err;
 
-    inquirer.prompt([
-        {
-        name: "choice",
-        type: "input",
-        message: "What is the item ID of the item you would like to purchase?"
-        }
-    ]).then(function(answer){
-        let selection = answer.choice;
-        connection.query("SELECT * FROM products WHERE item_id=?", selection,  function(err, results) {
-            if(err) throw err;
-            if(results.length === 0){
-                console.log("That item does not exist. Please select an item ID from the options given");
-                userBuy();
-            }else{
-                inquirer.prompt({
-                    name: "quantity",
-                    type: "input",
-                    message: "How many would you like to purchase?"
-                }).then(function(answer1){
-                    let quantity = answer1.quantity;
-                    if(quantity > results[0].stock_quantity){
-                        console.log("We are sorry, supplies are limited. Please try again.");
-                        userBuy();
-                    }else{
-                        console.log("");
-                        console.log(results[0].product_name + " purchased.");
-                        console.log("Your total is " + parseInt(answer1.quantity) * parseInt(answer1.price))
-
-                        let newTotalItems = results[0].stock_quantity - quantity;
-                        connection.query("UPDATE products SET stock_quantity=? " + newTotalItems + "WHERE item_id=? " + results[0].item_id,
-                        function(err, resUpdate){
-                            if(err) throw err;
-                            console.log("Your order has been processed")
-                            connection.end();
-                        })
-
-                       
+        inquirer.prompt([
+            {
+                name: "choice",
+                type: "list",
+                choices: function () {
+                    var choiceArray = [];
+                    for (let i = 0; i < results.length; i++) {
+                        choiceArray.push(results[i].item_id.toString());
                     }
-                })
+                    return choiceArray;
+                },
+                message: "What is the item ID of the item you would like to purchase?"
+            },
+            {
+                name: "quantity",
+                type: "input",
+                message: "How many would you like to buy?"
             }
-            
-    })
-  });
+        ]).then(function (answer) {
+            var chosenItem;
+            for (let i = 0; i < results.length; i++) {
+                if (results[i].item_id === parseInt(answer.choice)) {
+                    chosenItem = results[i];
+
+                }
+
+            }
+            var totalAmount = chosenItem.price * answer.quantity;
+            if (chosenItem.stock_quantity > parseInt(answer.quantity)) {
+                connection.query(
+                    "UPDATE products SET ? WHERE ?",
+                    [
+                        {
+                            stock_quantity: chosenItem.stock_quantity - answer.quantity
+                        },
+                        {
+                            item_id: chosenItem.item_id
+                        }
+
+                    ],
+                    function (error) {
+                        if (error) throw err;
+                        console.log("Purchase was successful! Your total is " + totalAmount);
+                        userBuy();
+
+
+                    }
+                );
+
+            } else {
+                console.log("Not enough supply. Please try again.");
+                userBuy();
+            }
+        })
+    });
 }
